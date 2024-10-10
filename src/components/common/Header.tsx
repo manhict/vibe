15a9d6e36f0ad544d731b0945c4cfaa489c9fed0
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-11 03:58:53 +0530

    Update Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="border max-md:border-none max-md:rounded-none backdrop-blur-lg max-md:w-full w-7/12 p-3 max-md:py-1 rounded-xl px-5 z-40 border-[#49454F] flex items-center justify-between ">
+    <header className="border max-md:border-none max-md:rounded-none backdrop-blur-lg max-md:w-full w-7/12 p-3 rounded-xl px-5 z-40 border-[#49454F] flex items-center justify-between ">
       <Link href={"/"} className=" cursor-pointer">
         <Image
           src={"/logo.svg"}
