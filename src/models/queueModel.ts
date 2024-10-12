3f39a939217cdf31193ad7755ca5d419de1f05e0
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-12 23:48:57 +0530

    Update queueModel.ts

--- src/models/queueModel.ts
+++ src/models/queueModel.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Define the queue schema
 const queueSchema = new mongoose.Schema(
   {
     roomId: {
@@ -7,13 +8,47 @@ const queueSchema = new mongoose.Schema(
       ref: "Room",
       required: true,
     },
+    isPlaying: {
+      type: Boolean,
+      default: false,
+    },
     songData: {
-      type: mongoose.Schema.Types.Mixed,
-      required: true,
+      id: { type: String, required: true },
+      name: { type: String, required: true },
+      artists: {
+        primary: [
+          {
+            id: { type: Number, required: false },
+            name: { type: String, required: false },
+            role: { type: String, required: false },
+            image: { type: [String], required: false }, // Array of image URLs
+            type: { type: String, enum: ["artist"], required: false },
+            url: { type: String, required: true },
+          },
+        ],
+      },
+      image: [
+        {
+          quality: { type: String, required: false },
+          url: { type: String, required: false },
+        },
+      ],
+      downloadUrl: [
+        {
+          quality: { type: String, required: false },
+          url: { type: String, required: false },
+        },
+      ],
+      addedBy: { type: String, required: false },
+      queueId: { type: String, required: false },
+      voteCount: { type: Number, required: true, default: 0 },
+      topVoters: { type: [String], required: false }, // Array of user IDs
+      isVoted: { type: Boolean, required: false, default: false },
     },
   },
   { timestamps: true }
 );
 
+// Create or use the Queue model
 const Queue = mongoose.models?.Queue || mongoose.model("Queue", queueSchema);
 export default Queue;
