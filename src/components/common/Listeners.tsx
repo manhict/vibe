a7b53b641de1d9707bd5f980f86d769be7d2d357
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-13 15:36:27 +0530

    Update Listeners.tsx

--- src/components/common/Listeners.tsx
+++ src/components/common/Listeners.tsx
@@ -45,7 +45,7 @@ function Listeners({ className }: { className?: string }) {
   return (
     <Dialog>
       <DialogTrigger
-        disabled={listener?.totalUsers === 1}
+        disabled={listener?.totalUsers === 0}
         className={cn(
           " max-md:w-full flex items-center text-sm font-medium justify-between",
           className
@@ -117,7 +117,7 @@ function Listeners({ className }: { className?: string }) {
           <div className="flex flex-col w-full overflow-hidden rounded-2xl">
             <div className="bg-black/80 w-full p-2.5 px-4">
               <p className=" font-semibold">
-                {listener && `${listener?.totalUsers - 1}`} Listeners
+                {listener && `${listener?.totalUsers}`} Listeners
               </p>
             </div>
             <div className="bg-black/80 flex-col flex items-center gap-2 justify-between p-2.5 pt-0 px-4 max-h-[50dvh] overflow-y-scroll">
