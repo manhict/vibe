026149a1e8594d22a5d6789018f779d330904320
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-25 22:49:06 +0530

    Update roomUsers.ts

--- src/models/roomUsers.ts
+++ src/models/roomUsers.ts
@@ -4,7 +4,7 @@ const roomUserSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Users",
       required: true,
     },
     socketid: {
