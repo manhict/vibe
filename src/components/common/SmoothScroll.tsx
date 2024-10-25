f2a408231a10b5fec67e1e1c36c4ff9785ec5423
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-26 01:55:35 +0530

    Update SmoothScroll.tsx

--- src/components/common/SmoothScroll.tsx
+++ src/components/common/SmoothScroll.tsx
@@ -1,45 +1,45 @@
 "use client";
-import { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
+// import { useEffect, useRef } from "react";
+// import Lenis from "@studio-freight/lenis";
 
 function SmoothScrollingDiv({ children }: { children: React.ReactNode }) {
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const lenisRef = useRef<Lenis | null>(null);
+  // const scrollRef = useRef<HTMLDivElement>(null);
+  // const lenisRef = useRef<Lenis | null>(null);
 
-  useEffect(() => {
-    if (!scrollRef.current) return;
+  // useEffect(() => {
+  //   if (!scrollRef.current) return;
 
-    // Initialize Lenis with the scrollable div as the wrapper
-    const lenis = new Lenis({
-      wrapper: scrollRef.current,
-    });
+  //   // Initialize Lenis with the scrollable div as the wrapper
+  //   const lenis = new Lenis({
+  //     wrapper: scrollRef.current,
+  //   });
 
-    lenisRef.current = lenis;
+  //   lenisRef.current = lenis;
 
-    // Animation frame to continuously update Lenis
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+  //   // Animation frame to continuously update Lenis
+  //   function raf(time: number) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
 
-    requestAnimationFrame(raf);
+  //   requestAnimationFrame(raf);
 
-    // Clean up when the component unmounts
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+  //   // Clean up when the component unmounts
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
 
-  // Update Lenis after dynamic content changes (trigger reflow)
-  useEffect(() => {
-    if (lenisRef.current) {
-      lenisRef.current.resize(); // Recalculate scrolling after content change
-    }
-  }, [children]); // Re-run when the children (dynamic content) changes
+  // // Update Lenis after dynamic content changes (trigger reflow)
+  // useEffect(() => {
+  //   if (lenisRef.current) {
+  //     lenisRef.current.resize(); // Recalculate scrolling after content change
+  //   }
+  // }, [children]); // Re-run when the children (dynamic content) changes
 
   return (
     <div
-      ref={scrollRef}
+      // ref={scrollRef}
       className="h-full transition-all z-50 overflow-y-scroll"
     >
       {children}
