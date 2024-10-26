13cde28c1c73af595132401924e833536c4b0750
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-26 14:02:01 +0530

    Update AudioContext.tsx

--- src/app/store/AudioContext.tsx
+++ src/app/store/AudioContext.tsx
@@ -224,7 +224,7 @@ export const AudioProvider: React.FC<AudioProviderProps> = ({ children }) => {
       if (audioRef.current) {
         const currentTime = audioRef.current.currentTime;
 
-        if (Math.abs(currentTime - lastEmittedTime.current) >= 1) {
+        if (Math.abs(currentTime - lastEmittedTime.current) >= 1.1) {
           setProgress(currentTime);
           lastEmittedTime.current = currentTime;
         }
