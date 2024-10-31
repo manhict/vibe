ed01225d9ee9bd1aaefbb13ef9405792faf149e1
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-01 00:14:11 +0530

    Update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -27,7 +27,7 @@ function Profile({ user, roomId }: { user: TUser; roomId?: string }) {
       Room: roomId || "",
     };
     api.setAuthToken(user?.token || null);
-    toast.loading("Connecting...", { id: "connecting" });
+    toast.loading("Connecting...", { id: "connecting", duration: 2000 });
     socket.connect();
   }, [user, setUser, roomId]);
 
