18187c465232e58d5690bc0cda955f055b695180
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-04 14:29:57 +0530

    Update Login.tsx

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -17,6 +17,8 @@ import { spotifyUser, TUser } from "@/lib/types";
 import { LogIn } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
+import Link from "next/link";
+import { FaSpotify } from "react-icons/fa";
 
 function Login() {
   const { setUser, user } = useUserContext();
@@ -77,7 +79,7 @@ function Login() {
               <FcGoogle className=" size-5" />
               {loader ? "Signing in..." : "Continue with Google"}
             </Button>
-            {/* <Link
+            <Link
               href={`https://accounts.spotify.com/en/authorize?client_id=${
                 process.env.SPOTIFY_CLIENT_ID
               }&scope=user-read-private%20user-read-email&response_type=token&redirect_uri=${encodeURIComponent(
@@ -88,7 +90,7 @@ function Login() {
                 <FaSpotify className=" size-5" />
                 Continue with Spotify
               </Button>
-            </Link> */}
+            </Link>
           </div>
         </div>
       </DialogContent>
