15c4fe486695d74d3852f14f1028cd80627fcfc6
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-08 07:12:42 +0530

    Update Background.tsx

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,38 +7,40 @@ function Background() {
 
   return (
     <div className="h-dvh relative overflow-hidden md:flex flex-col items-center justify-center py-4 w-full">
-      {/* Video Background */}
-      <video
-        //@ts-expect-error: missing
-        ref={backgroundVideoRef}
-        muted
-        playsInline
-        title={currentSong?.name || ""}
-        height={300}
-        width={300}
-        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
-        src={
-          currentSong?.downloadUrl[
-            currentSong?.downloadUrl?.length - 1
-          ]?.url.startsWith("http")
-            ? currentSong?.downloadUrl[currentSong.downloadUrl.length - 1].url
-            : `${process.env.STREAM_URL}/${
-                currentSong?.downloadUrl[currentSong.downloadUrl.length - 1].url
-              }` || "/cache.jpg"
-        }
-      />
-
-      {/* Overlay Content */}
-      {/* <div
-        className="relative bg-cover transition-all duration-700 bg-center w-full h-full"
-        style={{
-          backgroundImage: `url('${
-            currentSong?.image[currentSong?.image?.length - 1]?.url ||
-            "/bg.webp"
-          }')`,
-        }}
-      >
-      </div> */}
+      {currentSong?.downloadUrl[
+        currentSong?.downloadUrl?.length - 1
+      ]?.url.startsWith("http") ? (
+        <video
+          //@ts-expect-error: missing
+          ref={backgroundVideoRef}
+          muted
+          playsInline
+          title={currentSong?.name || ""}
+          height={300}
+          width={300}
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+          src={
+            currentSong?.downloadUrl[
+              currentSong?.downloadUrl?.length - 1
+            ]?.url.startsWith("http")
+              ? currentSong?.downloadUrl[currentSong.downloadUrl.length - 1].url
+              : `${process.env.STREAM_URL}/${
+                  currentSong?.downloadUrl[currentSong.downloadUrl.length - 1]
+                    .url
+                }` || "/cache.jpg"
+          }
+        />
+      ) : (
+        <div
+          className="relative bg-cover transition-all duration-700 bg-center w-full h-full"
+          style={{
+            backgroundImage: `url('${
+              currentSong?.image[currentSong?.image?.length - 1]?.url ||
+              "/bg.webp"
+            }')`,
+          }}
+        ></div>
+      )}
     </div>
   );
 }
