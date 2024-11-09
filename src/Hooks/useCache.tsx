dc32dbc81954aa4369c1d31aa79f17236dbe60c5
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-09 22:35:33 +0530

    removed video cache for now 🔫

--- src/Hooks/useCache.tsx
+++ src/Hooks/useCache.tsx
@@ -1,11 +1,11 @@
 import { useAudio } from "@/store/AudioContext";
 import { useUserContext } from "@/store/userStore";
-import getURL, { cacheVideo } from "@/utils/utils";
+import getURL from "@/utils/utils";
 import { useCallback, useEffect } from "react";
 
 function useCache() {
   const { currentSong, videoRef, backgroundVideoRef } = useAudio();
-  const { upNextSongs, showVideo } = useUserContext();
+  const { showVideo } = useUserContext();
 
   const loadVideos = useCallback(async () => {
     if (!currentSong) return;
@@ -38,26 +38,26 @@ function useCache() {
     }
   }, [currentSong, videoRef, backgroundVideoRef]);
 
-  const cacheUpNextSong = useCallback(async () => {
-    for (const song of upNextSongs) {
-      if (song.source !== "youtube") return;
-      const videoUrl = getURL(song);
-      if (videoUrl) {
-        await cacheVideo(videoUrl, song.id);
-      }
-    }
-  }, [upNextSongs]);
+  // const cacheUpNextSong = useCallback(async () => {
+  //   for (const song of upNextSongs) {
+  //     if (song.source !== "youtube") return;
+  //     const videoUrl = getURL(song);
+  //     if (videoUrl) {
+  //       await cacheVideo(videoUrl, song.id);
+  //     }
+  //   }
+  // }, [upNextSongs]);
   useEffect(() => {
     if (currentSong && currentSong.source == "youtube") {
       loadVideos();
     }
   }, [currentSong, loadVideos, showVideo]);
 
-  useEffect(() => {
-    if (upNextSongs.length > 0) {
-      cacheUpNextSong();
-    }
-  }, [upNextSongs, cacheUpNextSong]);
+  // useEffect(() => {
+  //   if (upNextSongs.length > 0) {
+  //     cacheUpNextSong();
+  //   }
+  // }, [upNextSongs, cacheUpNextSong]);
   return;
 }
 
--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -20,6 +20,10 @@ function Profile({ user, roomId }: { user: TUser; roomId?: string }) {
   const { setUser } = useUserContext();
 
   useEffect(() => {
+    console.log(
+      "%cVibe is Sexy",
+      "color: #D0BCFF; font-size: 20px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);"
+    );
     setUser(user);
     if (!roomId) toast.error("Room ID is required");
     socket.io.opts.extraHeaders = {
