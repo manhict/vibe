60f99a3ba7103c61eba0d3b220a5540b1986ddbb
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-11 02:52:10 +0530

    Update Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,10 +4,10 @@ import Userprofile from "../Userprofile";
 
 function Header() {
   return (
-    <header className="border max-sm:w-full max-xl:w-10/12 max-lg:w-10/12  max-md:border-none max-md:rounded-none backdrop-blur-lg max-md:w-full w-7/12 p-3 rounded-xl px-5 z-40 border-[#49454F] flex items-center justify-between ">
+    <header className="border max-sm:w-full max-xl:w-10/12 max-lg:w-10/12  max-md:border-none max-md:rounded-none backdrop-blur-lg max-md:w-full w-7/12 p-3 rounded-xl px-5 z-40 border-white/15 flex items-center justify-between ">
       <Image
         src={"/logo.svg"}
-        className=" size-12"
+        className=" size-12 max-md:size-10"
         alt="logo"
         height={500}
         width={500}
