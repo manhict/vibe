6de883da081eec8261a25641e6422042bb53f05a
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-11 18:16:57 +0530

    Update AudioContext.tsx

--- src/store/AudioContext.tsx
+++ src/store/AudioContext.tsx
@@ -239,9 +239,15 @@ export const AudioProvider: React.FC<AudioProviderProps> = ({ children }) => {
       if (Math.abs(currentTime - lastEmittedTime.current) >= 1.0) {
         setProgress(currentTime);
         if (videoRef.current) {
+          if (videoRef.current.paused && !audioRef.current.paused) {
+            videoRef.current?.play();
+          }
           videoRef.current.currentTime = currentTime;
         }
         if (backgroundVideoRef.current) {
+          if (backgroundVideoRef.current.paused && !audioRef.current.paused) {
+            backgroundVideoRef.current?.play();
+          }
           backgroundVideoRef.current.currentTime = currentTime;
         }
         lastEmittedTime.current = currentTime;
