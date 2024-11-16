10e8ac51834fc5f8ecee43bb52103e1c12e80403
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-17 04:20:43 +0530

    Update Reconnect.tsx

--- src/components/common/Reconnect.tsx
+++ src/components/common/Reconnect.tsx
@@ -7,7 +7,7 @@ function Reconnect() {
   if (!hiddenTimeRef.current) return;
   if (hiddenTimeRef.current < BACKGROUND_APP_TIMEOUT) return;
   return (
-    <div className=" w-full inset-0 text-zinc-200 h-screen bg-black/70 backdrop-blur-sm z-50 absolute flex items-center flex-col justify-center font-semibold text-xl">
+    <div className=" w-full inset-0 max-md:px-5 max-md:text-lg text-zinc-200 h-screen bg-black/70 backdrop-blur-sm z-50 absolute flex items-center flex-col justify-center font-semibold text-xl">
       <p>
         (
         {`App was in background for more ${Math.floor(
