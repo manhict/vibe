4c7ad5d84975f940a40770a137d666d5fa9bb5a3
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-17 13:21:05 +0530

    Update useSocket.tsx

--- src/Hooks/useSocket.tsx
+++ src/Hooks/useSocket.tsx
@@ -80,7 +80,6 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
     []
   );
   const hiddenTimeRef = useRef<number>(0);
-  const fetchWhenComeBack = useRef<boolean>(false);
   const timerRef = useRef<number | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [page, setPage] = useState<number | null>(1);
@@ -120,9 +119,6 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
   }, []);
 
   const updateListeners = useCallback(async () => {
-    if (isActive.current) {
-      fetchWhenComeBack.current = true;
-    }
     if (!isActive.current) return;
     if (listenerControllerRef.current) {
       listenerControllerRef.current.abort();
@@ -189,9 +185,6 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
   );
 
   const updateQueue = useCallback(async () => {
-    if (isActive.current) {
-      fetchWhenComeBack.current = true;
-    }
     if (!isActive.current) return;
     if (total.current && queue.length >= total.current) return;
 
@@ -236,9 +229,6 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
   const handleUpdateQueue = useDebounce(updateQueue);
 
   const upNextSong = useCallback(async () => {
-    if (isActive.current) {
-      fetchWhenComeBack.current = true;
-    }
     if (!isActive.current) return;
     if (upNextSongControllerRef.current) {
       upNextSongControllerRef.current.abort();
@@ -260,9 +250,6 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
   const getUpNextSong = useDebounce(upNextSong);
 
   const UpdateQueue = useCallback(async () => {
-    if (isActive.current) {
-      fetchWhenComeBack.current = true;
-    }
     if (!isActive.current) return;
     // force update user so that if scrolling then it not reset queue
     setLoading(true);
@@ -351,11 +338,10 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
       const wasAwayForTooLong = hiddenTimeRef.current > BACKGROUND_APP_TIMEOUT;
 
       isActive.current = true;
-      if (wasAwayForTooLong && fetchWhenComeBack) {
+      if (wasAwayForTooLong) {
         await updateListeners();
         await UpdateQueue();
         hiddenTimeRef.current = 0;
-        fetchWhenComeBack.current = false;
         return;
       }
       hiddenTimeRef.current = 0;
