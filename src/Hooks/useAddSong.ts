923f602ef5a82f4c152fd134b372ad2623e08896
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-18 23:13:57 +0530

    Update useAddSong.ts

--- src/Hooks/useAddSong.ts
+++ src/Hooks/useAddSong.ts
@@ -1,17 +1,28 @@
 import api from "@/lib/api";
 import { emitMessage } from "@/lib/customEmits";
 import { searchResults } from "@/lib/types";
+import { useUserContext } from "@/store/userStore";
 import { useCallback } from "react";
 import { toast } from "sonner";
 
 function useAddSong() {
+  const { queue } = useUserContext();
   const addSong = useCallback(
     async (selectedSongs: searchResults[], roomId?: string | null) => {
       if (!roomId) return;
+      const uniqueSongs = selectedSongs.filter(
+        (song) => !queue.some((queuedSong) => queuedSong.id === song.id)
+      );
+
+      if (uniqueSongs.length === 0) {
+        toast.info("No new songs to add to the queue.");
+        return;
+      }
+
       toast.loading("Adding songs to queue", { id: "adding" });
       const added = await api.post(
         `${process.env.SOCKET_URI}/api/add?room=${roomId}`,
-        selectedSongs
+        uniqueSongs
       );
       if (added.success) {
         emitMessage("update", "update");
@@ -19,7 +30,7 @@ function useAddSong() {
       }
       toast.dismiss("adding");
     },
-    []
+    [queue]
   );
 
   return { addSong };
