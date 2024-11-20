4e2cbe11bbbe2c8cfba83fdfb4e8682c09657b97
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-20 09:09:11 +0530

    Update getLoggedInUser.ts

--- src/app/actions/getLoggedInUser.ts
+++ src/app/actions/getLoggedInUser.ts
@@ -12,6 +12,7 @@ export async function getLoggedInUser() {
         Authorization: `Bearer ${vibeId}`,
         cookie: `room=${room} `,
       },
+      cache: "no-cache",
     });
     if (!res.ok) throw new Error(`Failed to fetch user: ${res.status}`);
     const data = await res.json();
