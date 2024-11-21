26a9cdad63eb884c004faed120d2d23edb82fc06
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-22 05:20:41 +0530

    Update Logo.tsx

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { cn } from "@/lib/utils";
@@ -7,8 +6,28 @@ import Image from "next/image";
 import React from "react";
 
 function LogoComp({ className }: { className?: string }) {
-  // const { user } = useUserContext();
-  return <p className=" text-2xl font-semibold">Vibe </p>;
+  const { user } = useUserContext();
+  return (
+    <div
+      onClick={() => {
+        if (!user) {
+          window.location.href = "/";
+        }
+      }}
+    >
+      <p className=" text-2xl md:hidden font-semibold">Vibe </p>
+      <Image
+        src={"/logo.svg"}
+        className={cn(
+          `size-12 max-md:hidden max-md:size-10 ${!user && ""}`,
+          className
+        )}
+        alt="logo"
+        height={500}
+        width={500}
+      />
+    </div>
+  );
 }
 const Logo = React.memo(LogoComp);
 export default Logo;
