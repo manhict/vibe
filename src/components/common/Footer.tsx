d8bee363b6f5c2879f91f0408e0da8c6a23fbc12
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-22 00:10:36 +0530

    Update Footer.tsx

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,12 +1,23 @@
 "use client";
-import { InstagramLogo } from "@phosphor-icons/react/dist/icons/InstagramLogo";
-import { XLogo } from "@phosphor-icons/react/dist/icons/XLogo";
 import Image from "next/image";
 import Link from "next/link";
 
 function Footer() {
   return (
     <footer className="border max-md:border-none max-xl:w-11/12 max-lg:w-11/12 border-white/15 max-sm:w-full backdrop-blur-lg  max-md:w-full select-none w-7/12 p-3 rounded-xl px-5 z-40 flex items-center justify-between ">
+      <div className=" flex -ml-1 text-2xl font-semibold gap-2">
+        <p className=" mt-2">Just</p>{" "}
+        <span>
+          <Image
+            src={"/logo.svg"}
+            alt="logo"
+            height={45}
+            className=" size-10"
+            width={45}
+          />
+        </span>
+        <p className=" mt-2">together.</p>
+      </div>
       <div className=" flex -ml-1 max-md:text-base text-xl items-center font-medium gap-2">
         <p className=" ">Built by </p>{" "}
         <span>
@@ -38,22 +49,6 @@ function Footer() {
             />
           </Link>
         </span>
-      </div>
-
-      <div className="flex text-xl md:text-2xl  items-center gap-2">
-        <Link href="https://www.instagram.com/fixing_x/" target="_blank">
-          <InstagramLogo
-            // size={24}
-            className=" hover:text-white text-zinc-300 transition-all duration-150"
-          />
-        </Link>
-
-        <Link href="https://twitter.com/tanmay11117" target="_blank">
-          <XLogo
-            // size={24}
-            className=" hover:text-white text-zinc-300 transition-all duration-150"
-          />
-        </Link>
       </div>
     </footer>
   );
