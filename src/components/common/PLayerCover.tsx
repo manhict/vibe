15ec249408cf142063cb325fb6e99013802dd2f0
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-23 21:21:20 +0530

    Update PLayerCover.tsx

--- src/components/common/PLayerCover.tsx
+++ src/components/common/PLayerCover.tsx
@@ -6,7 +6,13 @@ import { BsPip } from "react-icons/bs";
 import Image from "next/image";
 import UpvotedBy from "./UpvotedBy";
 function PLayerCoverComp() {
-  const { user, showVideo, setShowVideo } = useUserContext();
+  const {
+    user,
+    showVideo,
+    setShowVideo,
+    setShowDragOptions,
+    setShowAddDragOptions,
+  } = useUserContext();
   const { currentSong, videoRef } = useAudio();
   const [pip, setPIP] = useState<boolean>(false);
   const handleClick = useCallback(() => {
@@ -27,12 +33,16 @@ function PLayerCoverComp() {
 
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
     if (currentSong) {
+      setShowDragOptions(true);
+      setShowAddDragOptions(true);
       e.dataTransfer.setData("application/json", JSON.stringify(currentSong));
     }
   };
 
   const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
+    setShowDragOptions(false);
+    setShowAddDragOptions(false);
   };
   return (
     <div
