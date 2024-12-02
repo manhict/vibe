de9949e6f3e32ab298721e621628ee980268db5f
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-03 00:54:35 +0530

    Update InviteFriends.tsx

--- src/components/common/InviteFriends.tsx
+++ src/components/common/InviteFriends.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Listeners from "./Listeners";
-import { PiBookmarkSimpleBold } from "react-icons/pi";
+// import { PiBookmarkSimpleBold } from "react-icons/pi";
 import React from "react";
 import { cn } from "@/lib/utils";
 import InviteButton from "./inviteButton";
-import { Dialog, DialogTrigger } from "../ui/dialog";
+// import { Dialog, DialogTrigger } from "../ui/dialog";
+import Youtube from "./Youtube";
 
 function InviteFriendsComp({ className }: { className?: string }) {
   return (
@@ -12,14 +13,15 @@ function InviteFriendsComp({ className }: { className?: string }) {
       <Listeners className=" max-md:hidden" />
       <div className=" flex items-center gap-1">
         <InviteButton />
-        <Dialog>
+        <Youtube />
+        {/* <Dialog>
           <DialogTrigger
             disabled
             className=" items-center justify-center flex t h-8 rounded-lg px-2 text-xs bg-purple disabled:opacity-70 w-fit hover:bg-[#7140c5] hover:opacity-80 duration-300"
           >
             <PiBookmarkSimpleBold className=" size-4" />
           </DialogTrigger>
-        </Dialog>
+        </Dialog> */}
       </div>
     </div>
   );
