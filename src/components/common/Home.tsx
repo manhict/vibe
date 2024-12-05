11cca99c57057d51d49a0d3262f055a447fcc866
Ajay Sharma <traversities@gmail.com>
2024-12-06 02:26:59 +0530

    Fixed spacing units
    Updated design units &

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -110,7 +110,7 @@ export default function Home({
       >
         <Header user={user} roomId={roomId} />
 
-        <div className="max-md:w-full max-md:gap-0 h-full z-40 flex-wrap flex overflow-hidden max-xl:w-11/12 max-lg:w-11/12 max-sm:w-full max-md:overflow-scroll hide-scrollbar py-4 max-md:py-0 gap-4 w-7/12">
+        <div className="max-md:w-full max-md:gap-0 h-full z-40 flex-wrap flex overflow-hidden max-xl:w-11/12 max-lg:w-11/12 max-sm:w-full max-md:overflow-scroll hide-scrollbar py-4 max-md:py-0 gap-3 w-7/12">
           <Player />
 
           <AddToQueue />
