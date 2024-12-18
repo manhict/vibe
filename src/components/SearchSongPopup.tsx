12619f3b4d473080b229a4cc36164ab59b7b9a24
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-18 19:37:08 +0530

    Update SearchSongPopup.tsx

--- src/components/SearchSongPopup.tsx
+++ src/components/SearchSongPopup.tsx
@@ -322,7 +322,7 @@ function SearchSongPopupComp({
                 className=" bg-transparent flex md:hidden font-medium text-white p-2 w-full outline-none"
                 placeholder="Search songs"
               />
-              {!(window && (window.process as any)?.type) && (
+              {!window.navigator.userAgent.includes("Electron") && (
                 <svg
                   onClick={handleStarClick}
                   xmlns="http://www.w3.org/2000/svg"
